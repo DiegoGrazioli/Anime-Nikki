a72bd1e8c893a181283c0f57ac78786dc6fc857c
DiegoGrazioli <78029654@itiscastelli.onmicrosoft.com>
2024-03-28 17:51:36 +0100

    Added register page, fixed database and redirect, added graphic

--- app.py
+++ app.py
@@ -1,23 +1,67 @@
 #import requests
 from flask import Flask, redirect, request, render_template, url_for
-import sqlite3
+import sqlite3 as sq
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+def get_db():
+    conn = sq.connect('anime.sqlite3')
+    conn.row_factory = sq.Row
+    return conn
+
+@app.route('/')
 def index():
-    if request.method == 'POST':
+    return render_template('login.html')
 
+@app.route('/access', methods=['POST'])
+def login():
+    # Get the data from the form
+    usern = request.form.get('username')
+    passw = request.form.get('password')
 
-        # Then get the data from the form
-        username = request.form['username']
-        password = request.form['password']
+    # Connect to the database
+    conn = get_db()
+    cur = conn.cursor()
 
-        # Get the username/password associated with this tag
-        return render_template('home.html')
+    # Execute the query to check if the username and password match
+    cur.execute('SELECT * FROM users WHERE username = ? AND password = ?', (usern, passw))
+    result = cur.fetchone()
 
+    # Close the database connection
+    conn.close()
+
+    # Check if the result is not None, indicating a successful login
+    if result is not None:
+        return redirect('/home')
     else:
-        return render_template('login.html')
+        return render_template('login.html', message='Password o username errati. Riprova.')
+
+@app.route('/new_user')
+def go_register():
+    return render_template('register.html', message='')
+
+@app.route('/login')
+def go_login():
+    return render_template('login.html', message='')
+
+@app.route('/register', methods=['POST'])
+def register():
+    # Get the data from the form
+    usern = request.form.get('username')
+    passw = request.form.get('password')
+
+    # Connect to the database
+    conn = get_db()
+    cur = conn.cursor()
+
+    # Execute the query to check if the username and password match
+    cur.execute('INSERT INTO users (username, password) VALUES (?, ?)', (usern, passw))
+
+    # Close the database connection
+    conn.close()
+
+    # Check if the result is not None, indicating a successful login
+    return redirect('/home')
 
 @app.route('/home')
 def home():
